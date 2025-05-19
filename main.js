37e54a392a35b51586ec8bcf484565bd27afaad3
hiba ali <batulkhanzada111@gmail.com>
2025-05-19 22:19:28 +0500

    Creating Table

---
+++ main.js
@@ -0,0 +1,36 @@
+let body = document.getElementById('representive')
+let table = document.createElement("table")
+body.appendChild(table)
+const thead = document.createElement("thead")
+const tbody = document.createElement("tbody")
+const tfoot = document.createElement("tfoot")
+const tr = document.createElement("tr")
+const td = document.createElement("td")
+const td_2 = document.createElement("td")
+const td_3 = document.createElement("td")
+table.appendChild(thead)
+thead.appendChild(tr)
+tr.appendChild(td)
+tr.appendChild(td_2)
+tr.appendChild(td_3)
+td.innerHTML = "SNumber :"
+td_2.innerHTML="Name :"
+td_3.innerHTML="Roll Number"
+
+
+
+const tr_1 = document.createElement("tr")
+const col_1 = document.createElement("td")
+const col_2 = document.createElement("td")
+const col_3 = document.createElement("td")
+table.appendChild(tbody)
+tbody.appendChild(tr_1)
+tr_1.appendChild(col_1)
+tr_1.appendChild(col_2)
+tr_1.appendChild(col_3)
+col_1.innerHTML = "hello"
+col_2.innerHTML = "kitten"
+table.appendChild(tfoot)
+
+
+console.log(body)
